6275c53db8578f65384ed5e9bcae4d9a2b14ba4c
Siddharth Jain <jsiddharth847@gmail.com>
2024-12-20 09:56:43 +0530

    Add parking spot management routes
    Implemented POST /addparking route to add new parking spots.
    
    Commented out routes for editing, removing, and fetching available spots for future implementation.

---
+++ server/routes/parkingspot-router.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+
+const parkingSpotController = require("../Controllers/parkingspot-controller");
+
+// Routes for Parking Spot Management
+router.route("/addparking").post(parkingSpotController.addParkingSpot); // Add a new parking spot
+
+//router.route("/edit/:id").put(parkingSpotController.editParkingSpot); // Edit parking spot details
+
+//router.route("/remove/:id").delete(parkingSpotController.removeParkingSpot); // Remove a parking spot
+
+//router
+//.route("/available/:eventId")
+// .get(parkingSpotController.getAvailableSpots); // Get available spots for an event
+
+module.exports = router;
