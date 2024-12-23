8137099cb01f167c25349210da074b4cbc7930e3
Siddharth Jain <jsiddharth847@gmail.com>
2024-12-23 21:49:02 +0530

    adding parkingspot-router.js

--- server/routes/parkingspot-router.js
+++ server/routes/parkingspot-router.js
@@ -6,12 +6,12 @@ const parkingSpotController = require("../Controllers/parkingspot-controller");
 // Routes for Parking Spot Management
 router.route("/addparking").post(parkingSpotController.addParkingSpot); // Add a new parking spot
 
-//router.route("/edit/:id").put(parkingSpotController.editParkingSpot); // Edit parking spot details
+router.route("/edit/:id").put(parkingSpotController.editParkingSpot); // Edit parking spot details
 
-//router.route("/remove/:id").delete(parkingSpotController.removeParkingSpot); // Remove a parking spot
+router.route("/remove/:id").delete(parkingSpotController.removeParkingSpot); // Remove a parking spot
 
-//router
-//.route("/available/:eventId")
-// .get(parkingSpotController.getAvailableSpots); // Get available spots for an event
+router
+  .route("/available/:eventId")
+  .get(parkingSpotController.getAvailableSpots); // Get available spots for an event
 
 module.exports = router;
